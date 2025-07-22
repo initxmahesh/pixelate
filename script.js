480bfa3c8cda135e35b0e08c6880c2711bc0782b
im-mahesh1 <maheshchaudhary2057@gmail.com>
2025-07-22 13:33:50 +0545

    pixelate

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-// Your code here
+const table = document.querySelector('table');
+const addRow = document.getElementById('add-row');
+
+
+addRow.addEventListener('click', function () {
+    makeRow();
+})
+
+function makeRow() {
+    const tableRow = document.createElement('tr');
+    for (let i = 0; i <60; i++){
+        const td = document.createElement('td');
+        tableRow.appendChild(td);
+    }
+    table.appendChild(tableRow);
+}
+
+let clrs = 'red';
+
+table.addEventListener('click', colorize);
+
+function colorize(event) {
+    const target = event.target
+    if (target.tagName !== "TD") {
+      return
+    }
+    if (target.className === clrs) {
+        target.className = '';
+    } else {
+        target.className = clrs;
+    }
+}
+
+const colors = document.getElementsByTagName('select')[0];
+colors.addEventListener('change', function (event) {
+    clrs = event.target.value;
+})
+
+table.addEventListener("mousedown", addMouseOver);
+function addMouseOver() {
+  table.addEventListener("mouseover", colorize);
+}
+
+
+table.addEventListener("mouseup", removeMouseOver);
+function removeMouseOver() {
+  table.removeEventListener("mouseover", colorize);
+}
